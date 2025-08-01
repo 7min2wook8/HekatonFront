af9e0e6aa67e8530482b39cbb7e13dacd07b2bbb
sonani <sonani3136@gmail.com>
2025-08-01 12:25:43 +0900

    layout console
    지도 출력 성공
    API키 연동방법 작성중

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,11 @@ import type React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "@/components/ui/sonner";
+import { KakaoMapProvider } from "@/contexts/kakao-map-context";
 import { AuthProvider } from "@/contexts/auth-context";
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import { Toaster } from "@/components/ui/sonner";
 import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,26 +25,14 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={inter.className}>
-        <AuthProvider>{children}</AuthProvider>
+        <KakaoMapProvider>
+          <AuthProvider>
+            {children}
+          </AuthProvider>
+        </KakaoMapProvider>
         <Toaster />
         {/* 카카오맵 API 스크립트 로드 */}
-        <Script
-          type="text/javascript"
-          strategy="beforeInteractive"
-          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&libraries=services&autoload=false`}
-
-          onLoad={() => {
-            console.log("스크립트의 문제 없음!");
-            if (window.kakao) {
-              console.log("window.kakao 객체가 생성됨!", window.kakao);
-            } else {
-              console.warn("스크립트가 올바르게 로드되었으나 window.kakao 객체를 찾을 수 없습니다.");
-            }
-          }}
-          onError={(e) => {
-            console.error("카카오맵 스크립트 로드에 실패했습니다.", e);
-          }}
-        ></Script>
+        
       </body>
     </html>
   );
